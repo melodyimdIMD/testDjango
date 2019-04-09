2fdf7856f932da5e94e40b330e7de310c345024e
ljq <ljq@BF>
2019-04-09 17:07:14 +0800

    1

---
+++ project_name/hello.py
@@ -0,0 +1,38 @@
+from django.http import HttpResponse
+import sys
+from django.conf.urls import url
+from django.conf import settings
+from django.core.wsgi import get_wsgi_application
+
+import os
+
+DEBUG = os.environ.get('DEBUG','on') == 'on'
+#SECRET_KEY = os.environ.get('SECRET_KEY',os.urandom(32))
+SECRET_KEY = os.environ.get('SECRET_KEY','{{secret_key}}')
+
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS','localhost').split(',')
+
+settings.configure(
+    DEBUG=DEBUG,
+    SECRET_KEY=SECRET_KEY,
+    ALLOWED_HOSTS=ALLOWED_HOSTS,
+    ROOT_URLCONF=__name__,
+    MIDDLEWARE_CLASSES=(
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    ),
+)
+
+def index(request):
+    return HttpResponse("Hello Wold")
+
+urlpatterns = ( 
+                url(r'^$',index),
+                )
+application = get_wsgi_application()
+
+if __name__ == '__main__':
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
